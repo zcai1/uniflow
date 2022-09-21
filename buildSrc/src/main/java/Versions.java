b79d2a6a3d7526ca98cda7fd8ff301c2192974a1
zcai <z44cai@uwaterloo.ca>
2022-09-20 23:22:39 -0400

    update build dependencies

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,10 +2,11 @@
 class Versions {
     public static final String IDEA_EXT = "1.1.5";
     public static final String SHADOW = "7.1.2";
-    public static final String CHECKER_FRAMEWORK = "3.22.0";
+    public static final String CHECKER_FRAMEWORK = "3.23.0-eisop2";
     public static final String GUAVA = "23.5-jre";
     public static final String JUNIT = "5.8.2";
     public static final String AUTO_VALUE = "1.9";
+    public static final String AUTO_SERVICE = "1.0.1";
     public static final String GOOGLE_JAVA_FORMAT = "1.15.0";
     public static final String SPOTLESS = "6.7.2";
     public static final String JCOMMANDER = "1.82";
