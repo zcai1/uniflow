fe2182b9ac9aacfffa05f9f637642c5a078d636e
zcai <z44cai@uwaterloo.ca>
2022-11-08 14:36:07 -0500

    update cf version

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,7 +2,7 @@
 class Versions {
     public static final String IDEA_EXT = "1.1.5";
     public static final String SHADOW = "7.1.2";
-    public static final String CHECKER_FRAMEWORK = "3.23.0-eisop2";
+    public static final String CHECKER_FRAMEWORK = "3.27.0-eisop1";
     public static final String GUAVA = "23.5-jre";
     public static final String JUNIT = "5.8.2";
     public static final String AUTO_VALUE = "1.9";
