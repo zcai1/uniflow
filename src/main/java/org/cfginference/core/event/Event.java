5ebb965dd09b65becceefa82965d51e766868909
zcai <z44cai@uwaterloo.ca>
2023-01-10 00:33:25 -0500

    add new event for monitoring full analysis completion

--- src/main/java/org/cfginference/core/event/Event.java
+++ src/main/java/org/cfginference/core/event/Event.java
@@ -4,5 +4,9 @@ import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
 
 public interface Event {
+    enum SimpleEvent implements Event {
+        FULL_ANALYSIS
+    }
+
     record NewAnalysisTask(CompilationUnitTree root, ClassTree topLevelClass) implements Event {}
 }
