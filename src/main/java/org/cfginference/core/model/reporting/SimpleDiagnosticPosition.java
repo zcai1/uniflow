bffce34e72c7f559632a438920878250c8d7d8e7
zcai <z44cai@uwaterloo.ca>
2023-01-16 00:57:44 -0500

    prevent retaining tree in AnalysisMessage

---
+++ src/main/java/org/cfginference/core/model/reporting/SimpleDiagnosticPosition.java
@@ -0,0 +1,32 @@
+package org.cfginference.core.model.reporting;
+
+import com.google.auto.value.AutoValue;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.Tree;
+import com.sun.tools.javac.tree.EndPosTable;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.util.JCDiagnostic;
+
+@AutoValue
+public abstract class SimpleDiagnosticPosition implements JCDiagnostic.DiagnosticPosition {
+
+    public abstract int getEndPosition();
+
+    // Don't save tree to prevent memory leak.
+    @Override
+    public JCTree getTree() {
+        return null;
+    }
+
+    @Override
+    public int getEndPosition(EndPosTable endPosTable) {
+        return getEndPosition();
+    }
+
+    public static SimpleDiagnosticPosition create(CompilationUnitTree root, Tree tree) {
+        JCTree jcTree = (JCTree) tree;
+        return new AutoValue_SimpleDiagnosticPosition(jcTree.getStartPosition(),
+                jcTree.getPreferredPosition(),
+                jcTree.getEndPosition(((JCTree.JCCompilationUnit) root).endPositions));
+    }
+}
